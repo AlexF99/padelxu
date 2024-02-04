32359f93d6dbb59976383aa63312b272e56059d2
Alexandre Freitas <alexandreopf@gmail.com>
2024-02-04 14:36:08 -0300

    adding remove match button

--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -1,8 +1,9 @@
-import { Box, Button, Grid, Typography } from '@mui/material'
-import { collection, query, getDocs, orderBy } from "firebase/firestore";
+import { Box, Button, Fab, Grid, Typography } from '@mui/material'
+import { collection, query, getDocs, orderBy, deleteDoc, doc } from "firebase/firestore";
 import { db } from '../../firebase';
 import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
+import CloseIcon from '@mui/icons-material/Close';
 
 const Home = () => {
     const [matches, setMatches] = useState<any[]>([]);
@@ -21,6 +22,11 @@ const Home = () => {
         getMatches();
     }, [])
 
+    const removeMatch = async (matchId: string) => {
+        await deleteDoc(doc(db, "matches", matchId));
+        await getMatches();
+    }
+
     return (
         <Box className="PageContainer">
             <Button variant='contained'>
@@ -29,17 +35,22 @@ const Home = () => {
             {matches.map((item) => (
                 <Box key={item.id} className="ArrayContainer">
                     <Grid container spacing={2}>
-                        <Grid item xs={6}>
+                        <Grid item xs={5}>
                             <h3>time 1</h3>
                             <Typography variant="subtitle1" fontWeight="bold">{item.teamOne.players[0].name}</Typography>
                             <Typography variant="subtitle1" fontWeight="bold">{item.teamOne.players[1].name}</Typography>
                             <Typography variant="subtitle1" fontWeight="bold">{item.teamOne.points}</Typography>
                         </Grid>
-                        <Grid item xs={6}>
+                        <Grid item xs={5}>
                             <h3>time 2</h3>
                             <Typography variant="subtitle1" fontWeight="bold">{item.teamTwo.players[0].name}</Typography>
                             <Typography variant="subtitle1" fontWeight="bold">{item.teamTwo.players[1].name}</Typography>
                             <Typography variant="subtitle1" fontWeight="bold">{item.teamTwo.points}</Typography>
+                        </Grid>
+                        <Grid item xs={2} style={{ display: "flex", alignItems: "center", justifyContent: "center" }}>
+                            <Fab onClick={() => removeMatch(item.id)} size="small" color="error" aria-label="remove">
+                                <CloseIcon />
+                            </Fab>
                         </Grid>
                     </Grid>
                 </Box>
