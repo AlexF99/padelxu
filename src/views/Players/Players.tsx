c29c4348cac0fa8d1ec1b6caa0b152d14da10941
Alexandre Freitas <alexandreopf@gmail.com>
2024-02-04 21:43:54 -0300

    delete player button

--- src/views/Players/Players.tsx
+++ src/views/Players/Players.tsx
@@ -1,8 +1,10 @@
-import { Box, Typography } from "@mui/material";
+import { Box, Fab, Grid, Typography } from "@mui/material";
 import PlayerForm from "../../components/molecules/PlayerForm/PlayerForm";
 import { useEffect, useState } from "react";
 import { db } from "../../firebase";
-import { collection, getDocs, query } from "firebase/firestore";
+import { collection, deleteDoc, doc, getDocs, query } from "firebase/firestore";
+import CloseIcon from '@mui/icons-material/Close';
+
 
 const Players = () => {
     const [players, setPlayers] = useState<any[]>([]);
@@ -22,14 +24,29 @@ const Players = () => {
     }, [])
 
 
+    const removePlayer = async (playerId: string) => {
+        await deleteDoc(doc(db, "players", playerId));
+        await getPlayers();
+    }
+
     return (
         <Box className="PageContainer">
             <PlayerForm updatePlayers={getPlayers} />
             {(players && players.length) && players.map((item) => (
                 <Box key={item.id} className="ArrayContainer">
-                    <Typography variant="subtitle1" fontWeight="bold">
-                        {item.name}
-                    </Typography>
+                    <Grid container spacing={2}>
+                        <Grid item xs={9}>
+                            <Typography variant="subtitle1" fontWeight="bold">
+                                {item.name}
+                            </Typography>
+                        </Grid>
+                        <Grid item xs={3}>
+                            <Fab onClick={() => removePlayer(item.id)} size="small" color="error" aria-label="remove">
+                                <CloseIcon />
+                            </Fab>
+                        </Grid>
+                    </Grid>
+
                 </Box>
             ))}
         </Box>
