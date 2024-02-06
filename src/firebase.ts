b5428aa629db03de7fb8ea48cfca8cc2d15f65eb
Alexandre Freitas <alexandreopf@gmail.com>
2024-02-06 15:14:36 -0300

    moving firebase config to where it should be

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,15 +1,15 @@
-import {initializeApp} from 'firebase/app'
-import {getAuth} from 'firebase/auth'
-import {getFirestore} from 'firebase/firestore'
+import { initializeApp } from 'firebase/app'
+import { getAuth } from 'firebase/auth'
+import { getFirestore } from 'firebase/firestore'
 
 const firebaseConfig = {
-    apiKey: process.env.REACT_APP_FIREBASE_APIKEY,
-    authDomain: process.env.REACT_APP_FIREBASE_DOMAIN,
-    projectId: process.env.REACT_APP_FIREBASE_PROJECTID,
-    storageBucket: process.env.REACT_APP_FIREBASE_STORAGEBUCKET,
-    messagingSenderId: process.env.REACT_APP_FIREBASE_SENDERID,
-    appId: process.env.REACT_APP_FIREBASE_APPID,
-}
+    apiKey: "AIzaSyARqsp2QE0pRJfJneL4vg3xWAWYWKmrxs0",
+    authDomain: "xupadel-3bb3e.firebaseapp.com",
+    projectId: "xupadel-3bb3e",
+    storageBucket: "xupadel-3bb3e.appspot.com",
+    messagingSenderId: "848524545834",
+    appId: "1:848524545834:web:509c3423e8ceaed2bc5853"
+};
 
 const app = initializeApp(firebaseConfig)
 
